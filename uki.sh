3ea9db0084e0c80d8c8393a3e282f41a9fec8518
adelmonte <138840776+adelmonte@users.noreply.github.com>
2025-09-30 01:20:24 -0400

    Update uki.sh

--- uki.sh
+++ uki.sh
@@ -1,41 +1,39 @@
 #!/bin/bash
 
-# Root filesystem
-ROOT_FS_UUID="UUID= "
-ROOT_FS_DEVICE="/dev/mapper/luks"
-
-# General options
-GENERAL_OPTIONS="rw quiet loglevel=0 systemd.show_status=false udev.log_priority=3 vt.global_cursor_default=0"
-
-# Watchdog settings
-WATCHDOG_OPTIONS="nowatchdog watchdog=0 nmi_watchdog=0"
+# nvme0n1p2 partition (not filesystem) UUID
+PARTUUID=""
+# LUKS container
+CRYPT_UUID=""
 
 # Encryption and LUKS
-CRYPT_OPTIONS="cryptdevice=UUID= :luks- :allow-discards,no-read-workqueue,no-write-workqueue rd.luks.options=discard rd.luks.uuid="
+CRYPT_OPTIONS="cryptdevice=UUID=${CRYPT_UUID}:luks-${CRYPT_UUID}:allow-discards,no-read-workqueue,no-write-workqueue rd.luks.options=discard rd.luks.uuid=${CRYPT_UUID}"
+
+# General options
+GENERAL_OPTIONS="rw quiet loglevel=0 systemd.show_status=false udev.log_priority=3 vt.global_cursor_default=0 nowatchdog"
+# Enable old intel audio driver: snd-intel-dspcfg.dsp_driver=1
 
 # Power management and ACPI
-POWER_OPTIONS="pcie_aspm=force pcie_aspm.policy=powersupersave acpi_osi=\"Windows 2020\""
+POWER_OPTIONS="pcie_aspm=force pcie_aspm.policy=powersupersave acpi_osi=! acpi_osi=\"Windows 2015\""
+#POWER_OPTIONS="acpi.debug_level=0x2 acpi.debug_layer=0x0" (not working)
 
 # Graphics and display
 GRAPHICS_OPTIONS="drm.vblankoffdelay=1"
 
+# zswap options
+ZSWAP_OPTIONS="zswap.enabled=1 zswap.compressor=zstd zswap.max_pool_percent=90 zswap.zpool=zsmalloc zswap.same_filled_pages_enabled=1 zswap.max_compression_ratio=20"
+
 # Hibernate
-#resume=UUID= resume_offset= rootfstype=ext4
+# resume=UUID= resume_offset= rootfstype=xfs
 
-# Booster Initramfs
+# booster initramfs
 sudo /usr/lib/booster/regenerate_images
-
-# Move to the boot directory
-cd /boot
 
 # Build the ukify EFI file
 sudo ukify build \
---output=ukify-booster.efi \
---linux=vmlinuz-linux \
---microcode=intel-ucode.img \
---initrd=booster-linux.img \
---cmdline="${ROOT_FS_UUID} root=${ROOT_FS_DEVICE} ${GENERAL_OPTIONS} ${WATCHDOG_OPTIONS} ${CRYPT_OPTIONS} ${POWER_OPTIONS} ${GRAPHICS_OPTIONS}"
-
-cp -f /boot/ukify-booster.efi /boot/EFI/Linux/ukify-booster.efi
-
-
+--output=/boot/EFI/BOOT/BOOTX64.EFI \
+--splash=/boot/reward-splash.bmp \
+--linux=/boot/vmlinuz-linux-cachyos \
+--microcode=/boot/intel-ucode.img \
+--initrd=/boot/booster-linux-cachyos.img \
+--cmdline="UUID=${PARTUUID} root=/dev/mapper/luks-${CRYPT_UUID} ${CRYPT_OPTIONS} ${GENERAL_OPTIONS} ${POWER_OPTIONS} ${GRAPHICS_OPTIONS} ${ZSWAP_OPTIONS}" \
+&& sync
